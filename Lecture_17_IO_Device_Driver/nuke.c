04147c2ce21bfc174fbcafe2dd1c8d1afb530211
Xin Liu <xinliu1@umbc.edu>
2024-11-04 11:23:59 -0500

    driver

--- Lecture_17_IO_Device_Driver/nuke.c
+++ Lecture_17_IO_Device_Driver/nuke.c
@@ -10,7 +10,6 @@
 
 static int dev_major = 0;
 static struct class *lx_class = NULL;
-static struct cdev cdev;
 
 static ssize_t lx_read(struct file *, char __user *, size_t, loff_t *);
 static ssize_t lx_write(struct file *, const char __user *, size_t, loff_t *);
@@ -28,38 +27,66 @@ static struct nuke {
 static int __init lx_init(void) {
   dev_t dev;
   int i;
+  int ret;
 
-  // allocate device range
-  alloc_chrdev_region(&dev, 0, 1, "nuke");
+  // 分配设备号范围
+  ret = alloc_chrdev_region(&dev, 0, MAX_DEV, "nuke");
+  if (ret < 0) {
+    printk(KERN_ALERT "nuke: Failed to allocate char device region\n");
+    return ret;
+  }
 
-  // create device major number
+  // 获取主设备号
   dev_major = MAJOR(dev);
 
-  // create class
+  // 创建设备类
   lx_class = class_create(THIS_MODULE, "nuke");
+  if (IS_ERR(lx_class)) {
+    unregister_chrdev_region(MKDEV(dev_major, 0), MAX_DEV);
+    printk(KERN_ALERT "nuke: Failed to create class\n");
+    return PTR_ERR(lx_class);
+  }
 
   for (i = 0; i < MAX_DEV; i++) {
-    // register device
+    // 初始化 cdev
     cdev_init(&devs[i].cdev, &fops);
-    cdev.owner = THIS_MODULE;
-    cdev_add(&devs[i].cdev, MKDEV(dev_major, i), 1);
+    devs[i].cdev.owner = THIS_MODULE;
+
+    // 添加 cdev
+    ret = cdev_add(&devs[i].cdev, MKDEV(dev_major, i), 1);
+    if (ret) {
+      printk(KERN_ALERT "nuke: Failed to add cdev for device %d\n", i);
+      while (--i >= 0) {
+        cdev_del(&devs[i].cdev);
+        device_destroy(lx_class, MKDEV(dev_major, i));
+      }
+      class_destroy(lx_class);
+      unregister_chrdev_region(MKDEV(dev_major, 0), MAX_DEV);
+      return ret;
+    }
+
+    // 创建设备节点
     device_create(lx_class, NULL, MKDEV(dev_major, i), NULL, "nuke%d", i);
   }
   return 0;    
 }
 
 static void __exit lx_exit(void) {
-  device_destroy(lx_class, MKDEV(dev_major, 0));
-  class_unregister(lx_class);
+  int i;
+
+  for (i = 0; i < MAX_DEV; i++) {
+    device_destroy(lx_class, MKDEV(dev_major, i));
+    cdev_del(&devs[i].cdev);
+  }
   class_destroy(lx_class);
-  unregister_chrdev_region(MKDEV(dev_major, 0), MINORMASK);
+  unregister_chrdev_region(MKDEV(dev_major, 0), MAX_DEV);
 }
 
 static ssize_t lx_read(struct file *file, char __user *buf, size_t count, loff_t *offset) {
   if (*offset != 0) {
     return 0;
   } else {
-    uint8_t *data = "This is dangerous!\n";
+    const char *data = "This is dangerous!\n";
     size_t datalen = strlen(data);
     if (count > datalen) {
       count = datalen;
@@ -78,15 +105,17 @@ static ssize_t lx_write(struct file *file, const char __user *buf, size_t count,
     count = 4;
   }
 
-  copy_from_user(databuf, buf, count);
-  if (strncmp(buf, "\x01\x14\x05\x14", 4) == 0) {
+  if (copy_from_user(databuf, buf, count)) {
+    return -EFAULT;
+  }
+  if (strncmp(databuf, "\x01\x14\x05\x14", 4) == 0) {
     const char *EXPLODE[] = {
       "    ⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⣀⣀⠀⠀⣀⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
       "    ⠀⠀⠀⣀⣠⣤⣤⣾⣿⣿⣿⣿⣷⣾⣿⣿⣿⣿⣿⣶⣿⣿⣿⣶⣤⡀⠀⠀⠀⠀",
       "    ⠀⢠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀",
       "    ⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⡀⠀",
       "    ⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀",
-      "    ⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠟⠁⠀",
+      "    ⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠟⠁⠀",
       "    ⠀⠀⠻⢿⡿⢿⣿⣿⣿⣿⠟⠛⠛⠋⣀⣀⠙⠻⠿⠿⠋⠻⢿⣿⣿⠟⠀⠀⠀⠀",
       "    ⠀⠀⠀⠀⠀⠀⠈⠉⣉⣠⣴⣷⣶⣿⣿⣿⣿⣶⣶⣶⣾⣶⠀⠀⠀⠀⠀⠀⠀⠀",
       "    ⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠋⠈⠛⠿⠟⠉⠻⠿⠋⠉⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀",
@@ -100,10 +129,10 @@ static ssize_t lx_write(struct file *file, const char __user *buf, size_t count,
     int i;
 
     for (i = 0; i < sizeof(EXPLODE) / sizeof(EXPLODE[0]); i++) {
-      printk("\033[01;31m%s\033[0m\n", EXPLODE[i]);
+      printk(KERN_INFO "\033[01;31m%s\033[0m\n", EXPLODE[i]);
     }
   } else {
-    printk("nuke: incorrect secret, cannot lanuch.\n");
+    printk(KERN_INFO "nuke: incorrect secret, cannot launch.\n");
   }
   return count;
 }
