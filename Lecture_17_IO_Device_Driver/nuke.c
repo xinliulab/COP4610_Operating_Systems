f810df9f51e335190bb6e4a85633f27a65cf400e
Xin Liu <xinliu1@umbc.edu>
2024-11-06 11:22:03 -0500

    driver

--- Lecture_17_IO_Device_Driver/nuke.c
+++ Lecture_17_IO_Device_Driver/nuke.c
@@ -37,7 +37,7 @@ static int __init lx_init(void) {
 
   dev_major = MAJOR(dev);
 
-  lx_class = class_create(THIS_MODULE, "nuke");
+  lx_class = class_create("nuke");
   if (IS_ERR(lx_class)) {
     unregister_chrdev_region(MKDEV(dev_major, 0), MAX_DEV);
     printk(KERN_ALERT "nuke: Failed to create class\n");
@@ -47,7 +47,6 @@ static int __init lx_init(void) {
   for (i = 0; i < MAX_DEV; i++) {
     cdev_init(&devs[i].cdev, &fops);
     devs[i].cdev.owner = THIS_MODULE;
-
     ret = cdev_add(&devs[i].cdev, MKDEV(dev_major, i), 1);
     if (ret) {
       printk(KERN_ALERT "nuke: Failed to add cdev for device %d\n", i);
@@ -59,8 +58,6 @@ static int __init lx_init(void) {
       unregister_chrdev_region(MKDEV(dev_major, 0), MAX_DEV);
       return ret;
     }
-
-
     device_create(lx_class, NULL, MKDEV(dev_major, i), NULL, "nuke%d", i);
   }
   return 0;    
@@ -68,7 +65,6 @@ static int __init lx_init(void) {
 
 static void __exit lx_exit(void) {
   int i;
-
   for (i = 0; i < MAX_DEV; i++) {
     device_destroy(lx_class, MKDEV(dev_major, i));
     cdev_del(&devs[i].cdev);
@@ -95,22 +91,39 @@ static ssize_t lx_read(struct file *file, char __user *buf, size_t count, loff_t
 }
 
 static ssize_t lx_write(struct file *file, const char __user *buf, size_t count, loff_t *offset) {
-  char databuf[4] = "\0\0\0\0";
-  if (count > 4) {
-    count = 4;
+  char databuf[16] = {0};
+  size_t len;
+
+  if (count > sizeof(databuf) - 1) {
+    count = sizeof(databuf) - 1;
   }
 
   if (copy_from_user(databuf, buf, count)) {
     return -EFAULT;
   }
-  if (strncmp(databuf, "COP4610", 4) == 0) {
+
+  databuf[count] = '\0';
+
+  len = strlen(databuf);
+  while (len > 0 && (databuf[len - 1] == '\n' || databuf[len - 1] == '\r')) {
+    databuf[len - 1] = '\0';
+    len--;
+  }
+
+  printk(KERN_INFO "nuke: received input: '%s', length=%zu\n", databuf, len);
+
+  int cmp_result = strcmp(databuf, "COP4610");
+  printk(KERN_INFO "nuke: strcmp result: %d\n", cmp_result);
+
+  if (cmp_result == 0) {
+    printk(KERN_INFO "nuke: correct password entered.\n");
     const char *EXPLODE[] = {
       "    ⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⣀⣀⠀⠀⣀⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
       "    ⠀⠀⠀⣀⣠⣤⣤⣾⣿⣿⣿⣿⣷⣾⣿⣿⣿⣿⣿⣶⣿⣿⣿⣶⣤⡀⠀⠀⠀⠀",
       "    ⠀⢠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀",
-      "    ⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⡀⠀",
-      "    ⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀",
-      "    ⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠟⠁⠀",
+      "    ⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⡀⠀",
+      "    ⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀",
+      "    ⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢿⣿⣿⣿⣿⣿⣿⠿⠟⠁",
       "    ⠀⠀⠻⢿⡿⢿⣿⣿⣿⣿⠟⠛⠛⠋⣀⣀⠙⠻⠿⠿⠋⠻⢿⣿⣿⠟⠀⠀⠀⠀",
       "    ⠀⠀⠀⠀⠀⠀⠈⠉⣉⣠⣴⣷⣶⣿⣿⣿⣿⣶⣶⣶⣾⣶⠀⠀⠀⠀⠀⠀⠀⠀",
       "    ⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠋⠈⠛⠿⠟⠉⠻⠿⠋⠉⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀",
@@ -119,12 +132,11 @@ static ssize_t lx_write(struct file *file, const char __user *buf, size_t count,
       "    ⠀⠀⠀⠀⢰⣿⠛⠉⠉⠁⠀⠀⠀⢸⣿⣿⣧⠀⠀⠀⠀⠉⠉⠙⢻⣷⠀⠀⠀⠀",
       "    ⠀⠀⠀⠀⠀⠙⠻⠷⠶⣶⣤⣤⣤⣿⣿⣿⣿⣦⣤⣤⣴⡶⠶⠟⠛⠁⠀⠀⠀⠀",
       "    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-      "    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠒⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠓⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+      "    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠒⠛⠛⠛⠛⠛⠛⠛⠛⠛⠓⠀⠀⠀⠀⠀⠀⠀⠀⠀",
     };
     int i;
-
     for (i = 0; i < sizeof(EXPLODE) / sizeof(EXPLODE[0]); i++) {
-      printk(KERN_INFO "\033[01;31m%s\033[0m\n", EXPLODE[i]);
+      printk(KERN_INFO "%s\n", EXPLODE[i]);
     }
   } else {
     printk(KERN_INFO "nuke: incorrect secret, cannot launch.\n");
